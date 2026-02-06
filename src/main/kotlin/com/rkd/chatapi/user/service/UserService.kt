da7222aa1ae85f0e93cf36e5a8adc17d3775e626
alex052525 <alex62626@naver.com>
2026-02-06 11:00:34 +0900

    feat: 등록유저 검증 로직 분리

--- src/main/kotlin/com/rkd/chatapi/user/service/UserService.kt
+++ src/main/kotlin/com/rkd/chatapi/user/service/UserService.kt
@@ -2,17 +2,17 @@ package com.rkd.chatapi.user.service
 
 import com.rkd.chatapi.user.domain.entity.User
 import com.rkd.chatapi.user.domain.repository.UserRepository
+import com.rkd.chatapi.user.exception.ApiKeyAlreadyExistException
+import com.rkd.chatapi.user.validator.UserValidator
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val userValidator: UserValidator
 ) {
-    fun findOrCreateUserByApiKey(hashedApiKey: String): Long {
-        val existing = userRepository.findByApiKey(hashedApiKey)
-        if (existing != null && existing.id != null) {
-            return existing.id!!
-        }
+    fun createUserByApiKey(hashedApiKey: String): Long {
+        userValidator.validateUserNotRegistered(hashedApiKey)
 
         val user = User().apply {
             this.apiKey = hashedApiKey
