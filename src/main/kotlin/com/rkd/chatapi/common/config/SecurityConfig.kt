d44d9698a61882e45d3141ee0b8f7876be40e788
alex052525 <alex62626@naver.com>
2026-02-09 11:46:13 +0900

    refactor: SecurityConfig 패키지 이동

--- src/main/kotlin/com/rkd/chatapi/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/rkd/chatapi/common/config/SecurityConfig.kt
@@ -1,5 +1,7 @@
-package com.rkd.chatapi.common.security
+package com.rkd.chatapi.common.config
 
+import com.rkd.chatapi.common.security.ApiKeyAuthFilter
+import com.rkd.chatapi.common.security.JwtAuthFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
