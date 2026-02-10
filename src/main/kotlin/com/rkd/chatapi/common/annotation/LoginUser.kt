bf5a9031ce242d4e0864e23641b7291d38717c54
alex052525 <alex62626@naver.com>
2026-02-10 11:09:28 +0900

    feat: 인증 정보 어노테이션 구현

---
+++ src/main/kotlin/com/rkd/chatapi/common/annotation/LoginUser.kt
@@ -0,0 +1,8 @@
+package com.rkd.chatapi.common.annotation
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@AuthenticationPrincipal
+annotation class LoginUser
